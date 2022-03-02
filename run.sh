f8a0cecf8dda3d9f5650870fdc9b36b206fd1d92
ToSeeAll <qwer2846579@gmail.com>
2022-03-02 13:07:25 +0800

    修改最大内存

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-# 不可关闭命令行，关闭即停止程序，或使用 ctrl + c 命令停止程序
-java -Dfile.encoding=utf-8 -jar -Dserver.port=${PORT} zfile-release.jar
+# heroku最大内存好像就500M
+java -Xmx500m -Dfile.encoding=utf-8 -jar -Dserver.port=${PORT} zfile-release.jar
